0851ad960426580d604e68817d69976a11189c2f
Jh3bert <Jh3bert@Jh3bert-pc>
2019-08-26 21:39:30 -0400

    first attempt

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -5,10 +5,48 @@ import java.util.Scanner;
 public class A1Novice {
 
 	public static void main(String[] args) {
-		
+
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		int customerCount= 0;
+
+		System.out.println("Enter number of customers: ");
+		int customers = scan.nextInt();
+
+		String[] firstNames= new String[customers];
+		String[] lastNames= new String[customers];
+		double[] totals= new double[customers];
 		
+
+			for (int i =0; i<firstNames.length; i++) {
+			
+			
+			firstNames[i] = scan.next();
+			lastNames[i] = scan.next();
+						
+			int items = scan.nextInt();
+
+			for (int y =0; y<items; y++) {
+
+				int quantity = scan.nextInt();
+				
+				String itemName= scan.next();
+
+				double price = scan.nextDouble();
+				
+				totals[y]=quantity*price;
+			
+			}
+		
+			}
+			
+			for (int i =0; i<firstNames.length; i++) {
+				
+				System.out.println(firstNames[i].charAt(0)+". "+lastNames[i]+": "+totals[i]);
+			}
+	
+	
+	
 	}
 }
