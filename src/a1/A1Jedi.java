45a12ffd3ac5b7abeaa835216c377a4cd704ea7b
Jh3bert <Jh3bert@Jh3bert-pc>
2019-08-27 22:56:44 -0400

    first test

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -10,5 +10,49 @@ public class A1Jedi {
 
 		// Your code follows here.
 		
+		int storeItems = scan.nextInt();
+		
+		String[] itemNames = new String[storeItems];
+		double[] itemCosts = new double[storeItems];
+		
+		int[] itemQuantity = new int[storeItems];
+		int[] customerCount = new int[storeItems];
+		
+		for (int i =0; i<storeItems; i++) {
+			itemNames[i]= scan.next();
+			itemCosts[i]= scan.nextDouble();
+		}
+		
+		int customers = scan.nextInt();
+		
+		String[] firstNames = new String[customers];
+		String[] lastNames = new String[customers];
+		
+		for (int i=0; i<customers; i++) {
+			firstNames[i] = scan.next();
+			lastNames[i]= scan.next();
+			
+			int itemsBought = scan.nextInt();
+			
+			for(int y=0; y<itemsBought; y++) {
+				int quantity = scan.nextInt();
+				String itemName= scan.next();
+					
+				for (int x =0; x < itemNames.length; x++) {
+					if (itemNames[x].equalsIgnoreCase(itemName)) {
+						itemQuantity[x]+=quantity;
+						customerCount[x]+=1;
+					}
+				}
+			}
+	
+		}
+
+		for (int i=0; i < itemNames.length; i++) {
+			if (customerCount[i]==0)
+				System.out.println("No customers bought "+itemNames[i]);
+			else
+				System.out.println(customerCount[i]+" customers bought "+itemQuantity[i]+itemNames[i]);
+		}
 	}
 }
